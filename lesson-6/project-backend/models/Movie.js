9d2f0c15b147ea2c1ceca55b47d065d61244dbb8
unknown <bogdan.lyamzin.d@gmail.com>
2024-03-21 20:56:17 +0200

    finish lesson 6

---
+++ lesson-6/project-backend/models/Movie.js
@@ -0,0 +1,40 @@
+import { Schema, model } from "mongoose";
+
+import { handleSaveError, setUpdateSetting } from "./hooks.js";
+
+import { genreList, releaseYearRegexp } from "../constants/movie-constants.js";
+
+const movieSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    director: {
+        type: String,
+        required: true,
+    },
+    favorite: {
+        type: Boolean,
+        default: false,
+    },
+    genre: {
+        type: String,
+        enum: genreList,
+        required: true,
+    },
+    releaseYear: {
+        type: String,
+        match: releaseYearRegexp,
+        required: true,
+    }
+}, { versionKey: false, timestamps: true });
+
+movieSchema.post("save", handleSaveError);
+
+movieSchema.pre("findOneAndUpdate", setUpdateSetting);
+
+movieSchema.post("findOneAndUpdate", handleSaveError);
+
+const Movie = model("movie", movieSchema);
+
+export default Movie;
---
+++ lesson-6/project-backend/routes/moviesRouter.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+import moviesControllers from "../controllers/moviesControllers.js";
+
+import { movieAddSchema, movieUpdateSchema } from "../schemas/moviesSchemas.js";
+
+import validateBody from "../decorators/validateBody.js";
+
+import isValidId from "../middlewares/isValidId.js";
+
+const moviesRouter = express.Router();
+
+moviesRouter.get("/", moviesControllers.getAll);
+
+moviesRouter.get("/:id", isValidId, moviesControllers.getById);
+
+moviesRouter.post("/", validateBody(movieAddSchema), moviesControllers.add);
+
+moviesRouter.put("/:id", isValidId, moviesControllers.updateById);
+
+moviesRouter.delete("/:id", isValidId, moviesControllers.deleteById);
+
+export default moviesRouter;
