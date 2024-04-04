6b68d852dcd4d01c8de742a43daa47e202289614
unknown <bogdan.lyamzin.d@gmail.com>
2024-04-04 21:26:08 +0300

    finish lesson 10

---
+++ lesson-10/test-function-example/funcs/calcWeightIndex.js
@@ -0,0 +1,23 @@
+const calcWeightIndex = (weight, height) => {
+    if (weight === undefined || height === undefined) {
+        throw new Error('weight and height required');
+    }
+
+    if (typeof weight !== "number" || typeof height !== "number") {
+        throw new Error('weight and height must be number');
+    }
+
+    if (weight <= 0 || height <= 0) {
+        throw new Error('weight and height must be positive number');
+    }
+
+    if (weight < height) {
+        throw new Error('weight must be first argument and height - second');
+    }
+
+    const result = (weight / (height ** 2)).toFixed(2);
+
+    return Number(result);
+}
+
+export default calcWeightIndex;
